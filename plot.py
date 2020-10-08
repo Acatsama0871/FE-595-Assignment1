a5f0dd9dd8650ab656c1118f77168f3ae2ba896c
Gensoul66 <460856452@qq.com>
2020-10-07 23:51:51 -0400

    Add Tangent Ggraph

--- plot.py
+++ plot.py
@@ -5,13 +5,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def main():
+
     # define x series
     x = np.linspace(start=0, stop=2 * np.pi)
+
+    tan_x = np.linspace(start=0, stop=2 * np.pi)  # added by Baihao Huang
+    tan_y = np.tan(x)  # added by Baihao Huang
+    max_index = tan_y.argmax()  # added by Baihao Huang
+    min_idex = tan_y.argmin()  # added by Baihao Huang
+
+    tan_x[max_index] = np.nan  # added by Baihao Huang
+    tan_x[min_idex] = np.nan  # added by Baihao Huang
+    tan_y[max_index] = np.nan  # added by Baihao Huang
+    tan_y[min_idex] = np.nan  # added by Baihao Huang
 
     # plot
     plt.plot(x, np.sin(x), color="turquoise", label="sine")  # sine
     plt.plot(x, np.cos(x), color="moccasin", label="cosine")  # cosine
+    plt.plot(tan_x, tan_y, color="red", label="tan")  # added by Baihao Huang
+    plt.ylim(-1, 1)  # added by Baihao Huang
     plt.legend()
     plt.title("Sine and Cosine Plot")
     plt.xlabel("x")
@@ -19,5 +33,6 @@ def main():
     plt.grid()
     plt.show()
 
-if __name__ == "__main__":
+
+if __name__ == "__main__" :
     main()
